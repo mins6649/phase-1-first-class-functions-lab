085ed9758b9a410a78b40afbefd45495d32ebff1
minyoungshin <mins6649@gmail.com>
2023-02-01 16:41:56 -0500

    first class functions

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(array){
+    return array.slice(0,2);
+}
+const returnLastTwoDrivers = array => array.slice(-2);
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(int){
+    return fare => fare*int;
+}
+
+const fareDoubler = createFareMultiplier(2);
+//const fareDoubler = fare=>fare*2;
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(arrayOfScubers, selectingDrivers){
+    return selectingDrivers(arrayOfScubers);
+    //we are passing in the arrayOfScubers through our second argument which is a function. When this function is called ex: returnFirstTwoDrivers, it will take the arrayOfScubers and return the first two divers!
+
+}
